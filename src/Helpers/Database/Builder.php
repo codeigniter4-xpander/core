218124dab2ab25b9679b22df08dc9df05be52637
Bagus A Priambada <iam.adty@gmail.com>
2020-07-17 11:45:37 +0700

    database builder helper

---
+++ src/Helpers/Database/Builder.php
@@ -0,0 +1,41 @@
+<?php namespace CI4Xpander\Helpers\Database;
+
+class Builder {
+    public static function select($table = '', $fields = [], $prefix = true) {
+        $doPrefix = true;
+
+        if (is_bool($prefix)) {
+            $doPrefix = $prefix;
+            $prefix = $table;
+        } elseif (is_string($prefix)) {
+            if (empty($prefix)) {
+                $doPrefix = false;
+            } else {
+                $doPrefix = true;
+            }
+        } elseif (is_array($prefix)) {
+            
+        } else {
+            $doPrefix = false;
+        }
+        
+        $result = [];
+
+        foreach ($fields as $key => $field) {
+            $f = $field;
+            if (!is_numeric($key)) {
+                $f = $key;
+            }
+
+            if ($doPrefix) {
+                $prefixedField = $table . '.' . $f . ' ' . $prefix . '_' . $field;
+            } else {
+                $prefixedField = $table . '.' . $f . ' ' . $field;
+            }
+
+            $result[] = $prefixedField;
+        }
+
+        return $result;
+    }
+}
