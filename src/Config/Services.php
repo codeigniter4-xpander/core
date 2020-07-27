a69b48cae430a036f821b70999264076e91758d1
Bagus A Priambada <iam.adty@gmail.com>
2020-07-27 09:08:39 +0700

    service to hash password with algo from env

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -19,4 +19,27 @@ class Services extends \CodeIgniter\Config\Services
 
         return new \CI4Xpander\Model\Tracker();
     }
+
+    public static function hashPassword($password = '', $algo = null) {
+        if (is_null($algo)) {
+            switch (env('ci4xpander.dashboard.password_hash_algo', 'default')) {
+                case 'argon2id':
+                    $algo = PASSWORD_ARGON2ID;
+                break;
+                case 'argon2i':
+                    $algo = PASSWORD_ARGON2I;
+                break;
+                case 'bcrypt':
+                    $algo = PASSWORD_BCRYPT;
+                break;
+                default:
+                    $algo = PASSWORD_DEFAULT;
+                break;
+            }
+    
+            return $$algo;
+        }
+
+        return password_hash($password, $algo);
+    }
 }
