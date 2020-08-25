0d5d3c978bac8fd901d28a995b8a8d963dce91db
Bagus A Priambada <iam.adty@gmail.com>
2020-08-25 11:10:11 +0700

    database query builder helper

---
+++ src/Helpers/Database/Query/Builder.php
@@ -0,0 +1,77 @@
+<?php namespace CI4Xpander\Helpers\Database\Query;
+
+use CodeIgniter\Database\BaseBuilder;
+
+class Builder {
+    public static function select($table = '', $fields = [], $prefix = true) {
+        $doPrefix = true;
+
+        if (is_bool($prefix)) {
+            $doPrefix = $prefix;
+            $prefix = $table;
+        } elseif (is_string($prefix)) {
+            if (empty($prefix)) {
+                $doPrefix = false;
+            } else {
+                $doPrefix = true;
+            }
+        } elseif (is_array($prefix)) {
+
+        } else {
+            $doPrefix = false;
+        }
+
+        $result = [];
+
+        foreach ($fields as $key => $field) {
+            $f = $field;
+            if (!is_numeric($key)) {
+                $f = $key;
+            }
+
+            if ($doPrefix) {
+                $prefixedField = $table . '.' . $f . ' ' . $prefix . '_' . $field;
+            } else {
+                $prefixedField = $table . '.' . $f . ' ' . $field;
+            }
+
+            $result[] = $prefixedField;
+        }
+
+        return $result;
+    }
+
+    public static function escape($value = '')
+    {
+        return \Config\Database::connect()->escape($value);
+    }
+
+    public static function protect($identifier = '')
+    {
+        return \Config\Database::connect()->protectIdentifiers($identifier);
+    }
+
+    public static function subQuery($query, $alias)
+    {
+        if (is_a($query, \CodeIgniter\Database\BaseBuilder::class)) {
+            $query = $query->getCompiledSelect();
+        }
+
+        return "({$query}) {$alias}";
+    }
+
+    public static function forceQueryToString($query = null, ...$params)
+    {
+        if (is_a($query, BaseBuilder::class)) {
+            $query = $query->getCompiledSelect();
+        } elseif (is_callable($query)) {
+            $query = $query(...$params);
+        }
+
+        if (!is_string($query)) {
+            $query = self::forceQueryToString($query, ...$params);
+        }
+
+        return $query;
+    }
+}
